1f0a50a71996b637cb3ad32155d24ac5eab7000d
Jevin Anderson <jevinanderson@gmail.com>
2018-02-07 17:03:22 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # kraken devtools less 2.x
+
+### Configuration
+
+`less`, `sass`, `stylus`, `dustjs`, and a static `copier` plugin are available to use. To enable, set the `module` and `files` properties in your `config`, e.g.:
+
+```json
+{
+  "less-2.x": {
+    "module": "kraken-devtools-less-2.x",
+    "files": "/css/**/*.css",
+    "paths": ["./css", "../"],
+    "ext": "less"
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+var less = require('less');
+var path = require('path');
+
+module.exports = function(options) {
+  options = options || {};
+  options.ext = options.ext || 'less';
+  var paths = options.paths || ['.'];
+
+  return function(data, args, callback) {
+    var renderOptions = {
+      filename: args.context.name,
+      paths: paths.map(function(path) {
+        return path.resolve(args.context.srcRoot, pathOption);
+      })
+    };
+
+    less.render(data.toString('utf8'), renderOptions, function(error, output) {
+      if (error) {
+        callback(error);
+      }
+
+      callback(undefined, output.css);
+    });
+  };
+};
